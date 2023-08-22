5174f1a9f3dbbd9c499d9e911d235e01a5d99146
Luciano Muga <lucho.muga2@gmail.com>
2023-08-21 21:06:51 -0300

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath = path.resolve(__dirname,'./public')
 app.use(express.static(publicPath));
 
 
-app.listen(400, () => {
+app.listen(3000, () => {
     console.log("Servidor funcionando");
 })
 
